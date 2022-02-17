923846c20ff20645b00eeb3582d72bfc89ca262f
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-02-17 23:45:09 +0900

    collect, concat, lazy, replaceAll, removeIf

--- src/test/java/com/kiyotakeshi/examples/Playground3.java
+++ src/test/java/com/kiyotakeshi/examples/Playground3.java
@@ -3,12 +3,10 @@ package com.kiyotakeshi.examples;
 import com.kiyotakeshi.beans.Course;
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Playground3 {
 
@@ -20,6 +18,7 @@ public class Playground3 {
             new Course("gcp", "public cloud", 92, 50),
             new Course("azure", "public cloud", 81, 80),
             new Course("terraform", "IaC", 79, 250),
+            new Course("microservices", "Architecture", 98, 35),
             new Course("ansible", "IaC", 58, 13)
     );
 
@@ -107,5 +106,59 @@ public class Playground3 {
                                 Course::getCategory, Collectors.mapping(Course::getName, Collectors.toList())
                         ))
         );
+    }
+
+    @Test
+    void collect2() {
+        System.out.println(
+                courses.stream()
+                        .map(Course::getName)
+                        .collect(Collectors.joining(","))
+        );
+    }
+
+    @Test
+    void concat() {
+        List<Course> courses2 = List.of(
+                new Course("react", "javascript", 88, 440),
+                new Course("vue", "javascript", 68, 220),
+                new Course("flask", "python", 82, 150),
+                new Course("django", "python", 66, 380)
+        );
+
+        System.out.println(
+                Stream.concat(courses.stream(), courses2.stream()).collect(Collectors.toList())
+        );
+    }
+
+    @Test
+    void intermediateOperationIsLazy() {
+        System.out.println(
+                courses.stream()
+                        .map(Course::getName)
+                        .peek(System.out::println)
+                        .filter(c -> c.length() > 11)
+                        .map(String::toUpperCase)
+                        .peek(System.out::println)
+                        .findFirst()
+                        .orElse("no course over 11 characters")
+        );
+    }
+
+    @Test
+    void replaceAll() {
+        List<String> modifyCourses = courses.stream().map(Course::getName).collect(Collectors.toList());
+        System.out.println(modifyCourses);
+
+        modifyCourses.replaceAll(String::toUpperCase);
+        System.out.println(modifyCourses);
+    }
+
+    @Test
+    void removeIf() {
+        List<String> modifyCourses = courses.stream().map(Course::getName).collect(Collectors.toList());
+
+        modifyCourses.removeIf(c -> c.length() < 8);
+        System.out.println(modifyCourses);
     }
 }
